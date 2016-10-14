9a7897198ed92f4a07bfbc51651d544366a5ae41
Lucas <lucas@lucas-desktop.(none)>
2016-10-13 22:53:09 -0300

    Cambio de directorio del archivo HMMPNGS.hs

---
+++ HMMPNGS.hs
@@ -0,0 +1,42 @@
+module Data.HMMPNGS where
+
+import Data.HMM
+import Data.Word
+import Data.Array
+import Codec.Picture.Png
+import qualified Data.ByteString.Lazy as L
+{-
+data PngRawImage = PngRawImage
+    { header       :: PngIHdr
+    , chunks       :: [PngRawChunk]
+    }
+-}
+
+type Event = String
+
+png1 = ["PngRawImage",
+        "header = PngIHdr",
+        "colourType = PngGreyscale",
+        "colourType = PngTrueColour",
+        "colourType = PngIndexedColor",
+        "colourType = PngGreyscaleWithAlpha",
+        "colourType = PngTrueColourWithAlpha",
+        "interlaceMethod = PngNoInterlace",
+        "interlaceMethod = PngInterlaceAdam7"]
+
+mmpng1 = simpleMM png1 1
+
+{-Just PngRawImage {header = PngIHdr {width = 32,
+				    height = 32,
+				    bitDepth = 8,
+				    colourType = PngTrueColour,
+			 	    compressionMethod = 0,
+				    filterMethod = 0,
+				    interlaceMethod = PngNoInterlace},-}
+
+array1 = listArray (1,4) ["PngRawImage",
+                          "header = PngIHdr",
+                          "colourType = PngTrueColour",
+                          "interlaceMethod = PngNoInterlace"]
+
+mmpng2 = baumWelch mmpng1 array1
